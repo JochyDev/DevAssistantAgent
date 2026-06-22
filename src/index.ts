8c3152ce25ba2da897e7b354bc45f000eb986f03
JochyDev <joseluisjimenez1998@gmail.com>
2026-06-22 10:45:07 +0400

    feat: add streaming functionality for Claude responses with optional system prompts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 import { askClaude } from "./llm/anthropic-client.js";
+import { CODE_REVIEWER_PROMPT } from "./llm/prompts.js";
+
+const CODIGO_CON_PROBLEMAS = `
+async function getUser(id) {
+  const query = "SELECT * FROM users WHERE id = " + id;
+  const result = await db.query(query);
+  return result[0];
+}
+
+function calcularDescuento(precio, tipo) {
+  if (tipo == "vip") {
+    return precio * 0.8;
+  } else if (tipo == "regular") {
+    return precio * 0.9;
+  } else {
+    return precio;
+  }
+}
+`;
 
 async function main(): Promise<void> {
   console.log("╔════════════════════════════════════════╗");
@@ -6,14 +25,24 @@ async function main(): Promise<void> {
   console.log("║            Primera llamada             ║");
   console.log("╚════════════════════════════════════════╝");
   console.log("");
-  console.log(" Enviando pregunta a Cluade...");
+  console.log(" Demo 1: Enviando código SIN system prompt");
   console.log("");
-  const question = "¿Qué es Typescript y en qué se diferencia de Javascript? Responde máximo en 3 puntos concisos"
-  console.log(`❓ Pregunta: ${question}`);
+  const question = `Revisa este código:\n\`\`javascript\n ${CODIGO_CON_PROBLEMAS}\n\`\``;
+  // console.log(`❓ Pregunta: ${question}`);
   const answer = await askClaude(question)
   console.log("-".repeat(50));
   console.log(` Answer recibida: ${answer}`);
   console.log("-".repeat(50));
+   console.log("");
+
+  console.log(" Demo 2: Enviando código CON system prompt");
+  console.log("");
+  // console.log(`❓ Pregunta: ${question}`);
+  const reviewerPrompAnswer = await askClaude(question, CODE_REVIEWER_PROMPT)
+  console.log("-".repeat(50));
+  console.log(` Answer recibida: ${reviewerPrompAnswer}`);
+  console.log("-".repeat(50));
+  console.log("");
 }
 
 main().catch((error) => {
---
+++ src/llm/streaming.ts
@@ -0,0 +1,24 @@
+import { config } from "../config.js";
+import { client } from "./anthropic-client.js";
+
+async function streamClaude(prompt: string, systemPrompt?: string): Promise<string> {
+    let fullResponse = "";
+    const responseStream = await client.messages.stream({
+        model: config.anthropicModel,
+        max_tokens: 1024, 
+        ...(systemPrompt && { system: systemPrompt }),
+        messages: [
+            {
+                role: 'user',
+                content: prompt
+            }
+        ]
+    })
+    responseStream.on("text", (chunck) => {
+        process.stdout.write(chunck);
+        fullResponse += chunck;
+    });
+    await responseStream.finalMessage();
+    process.stdout.write("\n");
+    return fullResponse;
+}
